bb3da072897d962bce7802871813531b08e9d940
Dennise <dennise.macias@outlook.com>
2024-10-11 21:19:29 -0600

    Politicas, terminos y condiciones

---
+++ screens/staticfiles/politica-seguridad.js
@@ -0,0 +1,42 @@
+const  PoliticaSeguridad= `
+En Ophelia, la seguridad de su información es una prioridad. Nos comprometemos a proteger la confidencialidad, integridad y disponibilidad de los datos personales y transacciones realizadas en nuestra plataforma. Esta Política de Seguridad describe las medidas que tomamos para salvaguardar la información de los usuarios y el uso seguro de nuestra app.
+
+1. Seguridad de la Información
+Implementamos una serie de prácticas de seguridad para proteger su información, tanto personal como financiera, contra el acceso no autorizado, la pérdida, alteración, divulgación o destrucción. Entre las medidas que tomamos se incluyen:
+
+Cifrado de datos: Utilizamos el cifrado SSL (Secure Socket Layer) para proteger la transmisión de información sensible, como los detalles de pago y datos personales. Esto garantiza que su información esté encriptada y no sea accesible por terceros durante la transmisión.
+
+Control de acceso: Solo el personal autorizado de Ophelia tiene acceso a la información personal y financiera de los usuarios. Nuestro sistema garantiza que solo quienes tienen permisos específicos puedan manejar ciertos datos.
+
+Autenticación segura: Requerimos que los usuarios creen contraseñas seguras para acceder a sus cuentas. Además, recomendamos activar métodos de verificación adicionales, como la autenticación de dos factores, para mejorar la seguridad de su cuenta.
+
+2. Protección de Pagos
+En Ophelia, utilizamos proveedores de servicios de pago de confianza y certificados para gestionar transacciones financieras. Estos proveedores cumplen con los estándares de seguridad PCI-DSS (Payment Card Industry Data Security Standard), lo que asegura que los datos de su tarjeta de crédito o débito estén protegidos durante el proceso de compra.
+
+3. Monitoreo y Detección de Amenazas
+Realizamos un monitoreo constante de nuestra plataforma para identificar posibles vulnerabilidades o amenazas de seguridad. Además, contamos con sistemas automáticos de detección de intrusiones y malware para prevenir ataques cibernéticos. En caso de detectar actividad sospechosa, tomamos medidas inmediatas para mitigar riesgos y proteger los datos de nuestros usuarios.
+
+4. Actualización de Sistemas y Parcheo de Vulnerabilidades
+Nos aseguramos de que nuestra infraestructura tecnológica esté siempre actualizada con los parches y actualizaciones de seguridad más recientes. Esto incluye la actualización de servidores, software y bibliotecas de terceros para garantizar que la plataforma sea resistente frente a vulnerabilidades conocidas.
+
+5. Copias de Seguridad y Recuperación ante Desastres
+En Ophelia, realizamos copias de seguridad periódicas de nuestros sistemas y bases de datos para garantizar la continuidad del servicio en caso de incidentes imprevistos. También contamos con planes de recuperación ante desastres para restaurar la funcionalidad de la plataforma en el menor tiempo posible tras cualquier fallo grave.
+
+6. Responsabilidad del Usuario
+Aunque Ophelia toma medidas para garantizar la seguridad de la plataforma, también es fundamental que los usuarios adopten buenas prácticas de seguridad, como:
+
+Mantener sus credenciales de acceso confidenciales y no compartirlas con terceros.
+Usar contraseñas seguras, que incluyan una combinación de letras, números y símbolos.
+Evitar el uso de redes Wi-Fi públicas no seguras al realizar transacciones o ingresar información personal.
+Informarnos de inmediato si detecta cualquier actividad sospechosa en su cuenta.
+7. Notificación de Incidentes de Seguridad
+En caso de que ocurra una violación de seguridad que comprometa la información personal o financiera de nuestros usuarios, Ophelia se compromete a notificar a los usuarios afectados de manera oportuna y a implementar las acciones necesarias para mitigar el impacto de dicha violación.
+
+8. Cambios en la Política de Seguridad
+Ophelia se reserva el derecho de actualizar o modificar esta Política de Seguridad en cualquier momento. Cualquier cambio será publicado en nuestra app y, si es necesario, le notificaremos por correo electrónico o a través de la app. Le recomendamos revisar esta política periódicamente para estar al tanto de las últimas medidas de seguridad implementadas.
+
+9. Contacto
+Si tiene preguntas o preocupaciones sobre nuestra Política de Seguridad o si desea reportar una posible vulnerabilidad o incidente, puede contactarnos a través de [email de contacto] o mediante el formulario de contacto en nuestra app.
+
+`
+export default PoliticaSeguridad;
